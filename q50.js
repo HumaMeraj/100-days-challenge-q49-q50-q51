733745eb163b0816d71800a921930f0f8b887138
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-04-02 00:59:20 +0500

    File of q50

---
+++ q50.js
@@ -0,0 +1,3 @@
+//Multiline Template Literals: Use template literals to create a multiline string that describes your ideal day. Include at least three different activities.
+var myIdealDay = "My ideal day would involve:\n1.Waking up early and going for a jogging.\n2.Spending a few hours coding on a personal project.\n3.Ending the day by reading a good book.";
+console.log(myIdealDay);
---
+++ q50.ts
@@ -0,0 +1,9 @@
+//Multiline Template Literals: Use template literals to create a multiline string that describes your ideal day. Include at least three different activities.
+
+let myIdealDay = `My ideal day would involve:
+1.Waking up early and going for a jogging.
+2.Spending a few hours coding on a personal project.
+3.Ending the day by reading a good book.`;
+
+console.log(myIdealDay);
+
